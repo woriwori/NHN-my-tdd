5fa6ca90bea55d0208b62102ac7a31a5833e14e1
이원희 <wonhee.lee@nhntoast.com>
2020-10-27 11:10:22 +0900

    feat: 4장 예제 코드 변형

--- __test__/test.test.js
+++ __test__/test.test.js
@@ -2,11 +2,10 @@ import Dollar from '../src/Dollar';
 
 test('test multiplication', () => {
   const fiveInstance = new Dollar(5);
-  let productInstance = fiveInstance.times(2);
-  expect(productInstance.amount).toBe(10);
-  productInstance = fiveInstance.times(3);
-  expect(productInstance.amount).toBe(15);
+  expect(fiveInstance.times(2)).toEqual(new Dollar(10));
+  expect(fiveInstance.times(3)).toEqual(new Dollar(15));
 });
+
 test('test equality', () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
   expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
