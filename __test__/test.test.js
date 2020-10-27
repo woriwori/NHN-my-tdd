035b96a0ab8dbb2b131556b023617bf9ba0df794
이원희 <wonhee.lee@nhntoast.com>
2020-10-27 10:58:50 +0900

    feat: 1장 예제 코드 변형

--- __test__/test.test.js
+++ __test__/test.test.js
@@ -1,13 +1,6 @@
-test('곱하기 테스트', () => {
-  const calculator = {
-    add: (a, b) => a + b,
-  };
-
-  const spyFn = jest.spyOn(calculator, 'add');
-
-  const result = calculator.add(2, 3);
-
-  expect(spyFn).toBeCalledTimes(1);
-  expect(spyFn).toBeCalledWith(2, 3);
-  expect(result).toBe(5);
+import Dollar from '../src/Dollar';
+test('test multiplication', () => {
+  const fiveInstance = new Dollar(5);
+  fiveInstance.times(2);
+  expect(fiveInstance.amount).toBe(10);
 });
---
+++ src/Dollar.js
@@ -0,0 +1,8 @@
+export default class Dollar {
+  constructor(amount) {
+    this.amount = amount;
+  }
+  times(multiplier) {
+    this.amount *= multiplier;
+  }
+}
