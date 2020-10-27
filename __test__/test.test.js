089c5248af9b5b974b096bbb4cf08d384c69b687
이원희 <wonhee.lee@nhntoast.com>
2020-10-27 11:38:13 +0900

    feat: 8장 예제 코드 변형

--- __test__/test.test.js
+++ __test__/test.test.js
@@ -1,23 +1,22 @@
-import Dollar from '../src/Dollar';
-import Franc from '../src/Franc';
+import Money from '../src/Money';
 
 test('test multiplication', () => {
-  const fiveInstance = new Dollar(5);
-  expect(fiveInstance.times(2)).toEqual(new Dollar(10));
-  expect(fiveInstance.times(3)).toEqual(new Dollar(15));
+  const fiveInstance = Money.dollar(5);
+  expect(fiveInstance.times(2)).toEqual(Money.dollar(10));
+  expect(fiveInstance.times(3)).toEqual(Money.dollar(15));
 });
 
 test('test Franc multiplication', () => {
-  const fiveInstance = new Franc(5);
-  expect(fiveInstance.times(2)).toEqual(new Franc(10));
-  expect(fiveInstance.times(3)).toEqual(new Franc(15));
+  const fiveInstance = Money.franc(5);
+  expect(fiveInstance.times(2)).toEqual(Money.franc(10));
+  expect(fiveInstance.times(3)).toEqual(Money.franc(15));
 });
 
 test('test equality', () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-  expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
-  expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
-  expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+  expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+  expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
+  expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
+  expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
 
-  expect(new Franc(5).equals(new Dollar(6))).toBeFalsy();
+  expect(Money.franc(5).equals(Money.dollar(6))).toBeFalsy();
 });
--- src/Money.js
+++ src/Money.js
@@ -1,4 +1,10 @@
 export default class Money {
+  static dollar(amount) {
+    return new Dollar(amount);
+  }
+  static franc(amount) {
+    return new Franc(amount);
+  }
   equals(compare) {
     const moneyInstance = compare;
     return this.amount === moneyInstance.amount && this.constructor === moneyInstance.constructor;
